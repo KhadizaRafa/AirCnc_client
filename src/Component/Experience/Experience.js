acaf1f89b45867bf9d57aefaa79bdad2873d86cb
Khadiza Rafa <khadiza301rafa@gmail.com>
2021-04-13 04:34:50 +0600

    added experience carosuel dynamically

--- src/Component/Experience/Experience.js
+++ src/Component/Experience/Experience.js
@@ -1,52 +1,118 @@
 import React from 'react';
 import ExperienceCard from './ExperienceCard';
+import Carousel from "react-multi-carousel";
+import "react-multi-carousel/lib/styles.css";
 
 const Experience = () => {
-    const responsiveForExperience = {
-        superLargeDesktop: {
-          breakpoint: { max: 4000, min: 3000 },
-          items: 4
-        },
-        desktop: {
-          breakpoint: { max: 3000, min: 1024 },
-          items: 4
-        },
-        tablet: {
-          breakpoint: { max: 1024, min: 464 },
-          items: 3
-        },
-        mobile: {
-          breakpoint: { max: 464, min: 0 },
-          items: 1
+  const experienceData = [
+    {
+      name: 'NIGHTLIFE : NEW YORK',
+      info: "Discover the city's party scene",
+      price: 35,
+      views: 64
+    },
+    {
+      name: 'ENTERTAINMENT : VANCOUVER',
+      info: "Tour with an Enthusiastic local!",
+      price: 3,
+      views: 1
+    },
+    {
+      name: 'PHOTO CLASS : LOS ANGELES',
+      info: "Must have L.A. Pictures!",
+      price: 39,
+      views: 179
+    },
+    {
+      name: 'PHOTOGRAPHY : NEW YORK',
+      info: "Retro Photoshoot in NYC",
+      price: 49,
+      views: 72
+    }
+  ]
+  const houseData = [
+    {
+      name: 'NIGHTLIFE : NEW YORK',
+      info: "Unique Cob Cottage",
+      price: 128,
+      views: 284
+    },
+    {
+      name: 'ENTERTAINMENT : VANCOUVER',
+      info: "The Joshua Tree House",
+      price: 250,
+      views: 269
+    },
+    {
+      name: 'PHOTO CLASS : LOS ANGELES',
+      info: "A Pirate's Life For Me- Houseboat",
+      price: 209,
+      views: 140
+    }
+  ]
+  const responsiveForExperience = {
+    superLargeDesktop: {
+      breakpoint: { max: 4000, min: 3000 },
+      items: 4
+    },
+    desktop: {
+      breakpoint: { max: 3000, min: 1024 },
+      items: 4
+    },
+    tablet: {
+      breakpoint: { max: 1024, min: 464 },
+      items: 3
+    },
+    mobile: {
+      breakpoint: { max: 464, min: 0 },
+      items: 1
+    }
+  }
+  const responsiveForHome = {
+    superLargeDesktop: {
+      breakpoint: { max: 4000, min: 3000 },
+      items: 3
+    },
+    desktop: {
+      breakpoint: { max: 3000, min: 1024 },
+      items: 3
+    },
+    tablet: {
+      breakpoint: { max: 1024, min: 464 },
+      items: 2
+    },
+    mobile: {
+      breakpoint: { max: 464, min: 0 },
+      items: 1
+    }
+  }
+  return (
+    <section id="experience" className="mt-4">
+      <div className="d-flex justify-content-between">
+        <h5 className="m-2">Experiences</h5>
+        <small>See All</small>
+      </div>
+      <Carousel responsive={responsiveForExperience}>
+        {
+          experienceData.map(experience => <ExperienceCard experience={experience} />)
         }
-      }
+      </Carousel>
 
-      const responsiveForHome = {
-        superLargeDesktop: {
-          breakpoint: { max: 4000, min: 3000 },
-          items: 3
-        },
-        desktop: {
-          breakpoint: { max: 3000, min: 1024 },
-          items: 3
-        },
-        tablet: {
-          breakpoint: { max: 1024, min: 464 },
-          items: 2
-        },
-        mobile: {
-          breakpoint: { max: 464, min: 0 },
-          items: 1
+      <div className="d-flex justify-content-between">
+        <h5 className="m-2">Homes</h5>
+        <small>See All</small>
+      </div>
+      <Carousel responsive={responsiveForHome}>
+        {
+          houseData.map(house => <ExperienceCard experience={house} />)
         }
-      }
-    return (
-        <section id="experience" className="mt-4">
-            <ExperienceCard section_header={"Experiences"} responsive={responsiveForExperience}/>
-            <ExperienceCard section_header={"Homes"} responsive={responsiveForHome}/>
-             
-           
-        </section>
-    );
+      </Carousel>
+
+
+
+
+    </section>
+  );
 };
 
 export default Experience;
--- src/Component/Experience/ExperienceCard.js
+++ src/Component/Experience/ExperienceCard.js
@@ -1,38 +1,28 @@
 import React from 'react';
 import { Card } from 'react-bootstrap';
-import Carousel from "react-multi-carousel";
-import "react-multi-carousel/lib/styles.css";
 import sliderFive from "../../images/light-airy.jpg";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faStar } from '@fortawesome/free-solid-svg-icons'
 import './Experience.css'
 
-const ExperienceCard = ({section_header,responsive}) => {
+const ExperienceCard = ({experience}) => {
     return (
-        <section id="experience-card">
-            <div className="d-flex justify-content-between">
-                 <h5  className="m-2">{section_header}</h5> 
-                 <small>See All</small>     
-            </div>
-             <Carousel responsive={responsive}>
             <div className="m-2">
                 <Card className="experience_card_block">
                     <Card.Img variant="top" src={sliderFive} />
-                    <small className="font-weight-bold">NIGHTLIFE : NEW YORK</small>
+                    <small style={{fontSize: '10px'}}>{experience.name}</small>
                     <Card.Body className="p-0">
-                        <h6>Discover the city's party scene</h6>
-                        <small className="text-muted d-block">$35 per person</small>
+                        <p className="font-weight-bold">{experience.info}</p>
+                        <small className="text-muted d-block">${experience.price} per person</small>
                         <FontAwesomeIcon icon={faStar} size="xs" className="main-color"/> 
                         <FontAwesomeIcon icon={faStar} size="xs" className="main-color"/> 
                         <FontAwesomeIcon icon={faStar} size="xs" className="main-color"/> 
                         <FontAwesomeIcon icon={faStar} size="xs" className="main-color"/> 
                         <FontAwesomeIcon icon={faStar} size="xs" className="main-color"/> 
-                        <small className="text-muted"> 64</small>
+                        <small className="text-muted"> {experience.views}</small>
                     </Card.Body>
-                </Card>
+                </Card> 
             </div>
-        </Carousel>
-        </section>
     );
 };
 
